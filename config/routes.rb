0c1f562dfd9933073fcea9cc4bc64867cc4edcc0
tuong <tuong@tuong-Aspire.(none)>
2015-01-18 01:12:56 +0700

    Adding comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do 
+    resources :comments # only associated with posts
+  end
 
   root 'posts#index'
 end
